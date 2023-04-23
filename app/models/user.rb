1d0f8c838f5dc6e564a6f72aa23ef75eb1acf64d
Edward Heaver <edward.heaver01@gmail.com>
2023-04-23 15:07:44 +0100

    domain for photos makes sense now

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :items, dependent: :destroy
-  has_many :uploaded_photos, class_name: 'Photo',
-                             foreign_key: 'uploader_id',
-                             dependent: :nullify,
-                             inverse_of: 'uploader'
+  has_many :uploaded_photos, inverse_of: 'uploader', class_name: 'Photo',
+                             foreign_key: 'uploader_id', dependent: :nullify
 end
--- spec/factories/photos_factory.rb
+++ spec/factories/photos_factory.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :photo do
     association :imageable, factory: :item
+    association :uploader, factory: :user
     sequence(:url) { |n| "pic_#{n}.jpg" }
   end
 end

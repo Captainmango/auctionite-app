f8b1c67adda03d5b242f941c66f884ee47c0a4bf
Edward Heaver <edward.heaver01@gmail.com>
2023-04-24 20:00:42 +0100

    Sign in working

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@
 
 Rails.application.routes.draw do
   root 'landing#index', as: 'landing_page'
+
   get 'registration', action: 'new', controller: 'registration', as: 'new_user'
   post 'registration', action: 'register', controller: 'registration', as: 'sign_up'
+  get 'sign-in', action: 'new', controller: 'session', as: 'new_session'
+  post 'sign-in', action: 'sign_in', controller: 'session', as: 'sign_in'
+  post 'sign-out', action: 'sign_out', controller: 'session', as: 'sign_out'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

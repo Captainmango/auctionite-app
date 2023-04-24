aaf0ceb5e8cd09901011a9957bcec5dfc764b148
Edward Heaver <edward.heaver01@gmail.com>
2023-04-24 19:18:32 +0100

    tests and better validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   validates :email, :password, presence: true
-  validates :password, length: { maximum: 35 }
+  validates :password, length: { in: 6..35 }
 
   has_many :items, dependent: :destroy
   has_many :uploaded_photos, inverse_of: 'uploader', class_name: 'Photo',

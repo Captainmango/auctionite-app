a04f17a9968922e8cb74209bdbd5af43274769a8
Edward Heaver <edward.heaver01@gmail.com>
2023-04-26 19:20:58 +0100

    scaffold controller for items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :items
   root 'landing#index', as: 'landing_page'
 
   get 'registration', action: 'new', controller: 'registration', as: 'new_user'

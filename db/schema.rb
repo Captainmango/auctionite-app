6afebfb0670680bc3f703ef55e5011a218f0917d
Edward Heaver <edward.heaver01@gmail.com>
2023-04-30 21:03:17 +0100

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,46 +12,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_23_134856) do
+ActiveRecord::Schema[7.0].define(version: 20_230_423_134_856) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "items", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "description"
-    t.integer "starting_price"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
+  create_table 'items', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'description'
+    t.integer 'starting_price'
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_items_on_user_id'
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string "imageable_type", null: false
-    t.integer "imageable_id", null: false
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "uploader_id"
-    t.index ["imageable_type", "imageable_id"], name: "index_photos_on_imageable_type_and_imageable_id"
-    t.index ["uploader_id"], name: "index_photos_on_uploader_id"
+  create_table 'photos', force: :cascade do |t|
+    t.string 'imageable_type', null: false
+    t.integer 'imageable_id', null: false
+    t.string 'url'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'uploader_id'
+    t.index %w[imageable_type imageable_id], name: 'index_photos_on_imageable_type_and_imageable_id'
+    t.index ['uploader_id'], name: 'index_photos_on_uploader_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "crypted_password"
-    t.string "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "remember_me_token"
-    t.datetime "remember_me_token_expires_at", precision: nil
-    t.string "reset_password_token"
-    t.datetime "reset_password_token_expires_at", precision: nil
-    t.datetime "reset_password_email_sent_at", precision: nil
-    t.integer "access_count_to_reset_password_page", default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', null: false
+    t.string 'crypted_password'
+    t.string 'salt'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'remember_me_token'
+    t.datetime 'remember_me_token_expires_at', precision: nil
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_token_expires_at', precision: nil
+    t.datetime 'reset_password_email_sent_at', precision: nil
+    t.integer 'access_count_to_reset_password_page', default: 0
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['remember_me_token'], name: 'index_users_on_remember_me_token'
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token'
   end
-
 end

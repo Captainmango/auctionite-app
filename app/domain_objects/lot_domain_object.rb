f2ee5ba29c334fbe4ad2dee82f810239257b7804
Edward Heaver <edward.heaver01@gmail.com>
2023-08-27 14:49:59 +0100

    Bid logic. Just needs testing

--- app/domain_objects/lot_domain_object.rb
+++ app/domain_objects/lot_domain_object.rb
@@ -4,8 +4,13 @@ class LotDomainObject < ApplicationDomainObject
   uses_model :lot
 
   def bid(amount, user_id)
-    # @todo add logic in here to check the bid can actually be placed (amount should always be greater than max bid)
-    # if you are the highest bidder, you cannot place another bid. Only when you are not the highest bidder
+    highest_bid = bids.order('amount DESC').first
+
+    unless amount.to_f > highest_bid.amount && highest_bid.user.id != user_id
+      flash.now[:notice] = 'The highest bid was placed by you or is too low to be the new highest bid.'
+      return
+    end
+
     bids.create(timestamp: Time.now.utc, amount:, user_id:)
   end
 end

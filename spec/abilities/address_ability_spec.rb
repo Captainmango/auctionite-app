37fbda9780bd3d454cb851b64f6f00ae01283abf
Edward Heaver <edward.heaver01@gmail.com>
2023-10-01 13:49:49 +0100

    address owners can edit and delete address

---
+++ spec/abilities/address_ability_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'cancan/matchers'
+require 'rails_helper'
+
+RSpec.describe AddressAbility, type: :ability do
+  subject(:ability) { AddressAbility.new(user) }
+  describe 'Is the owner of an address' do
+    let(:user) { create(:user) }
+    let(:address) { create(:address, addressable_id: user.id) }
+
+    it { is_expected.to be_able_to(:read, address) }
+    it { is_expected.to be_able_to(:update, address) }
+    it { is_expected.to be_able_to(:destroy, address) }
+  end
+end
--- spec/factories/addresses_factory.rb
+++ spec/factories/addresses_factory.rb
@@ -2,5 +2,11 @@
 
 FactoryBot.define do
   factory :address do
+    house_no { rand(1..100) }
+    first_line { 'Mycroft street' }
+    county { 'Frogbrookshire' }
+    post_code { 'WF55 2WE' }
+    addressable_type { 'User' }
+    addressable_id { create(:user).id }
   end
 end

6f587e4b2f97db989238d7ab2d6cb1d9730e3918
Edward Heaver <edward.heaver01@gmail.com>
2023-10-01 13:27:18 +0100

    address assoc added

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Address < ApplicationRecord
+  belongs_to :addressable, polymorphic: true
+
+  validates :house_no, :first_line, :county, :post_code, presence: true
 end
